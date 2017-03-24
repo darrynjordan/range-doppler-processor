a574efe6e81e4837c76444f734953cea8ef07a83
darrynjordan <jrddar001@myuct.ac.za>
2017-03-24 13:51:30 +0200

    moved simulation files to dropbox

--- matlab_validation/get_frac_num.m
+++
@@ -1,8 +0,0 @@
-function [ fracNum ] = get_frac_num( Vco )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-fracNum = round((2^24)*((Vco*1e-6 + 2400)/25 - 96));
-
-end
-
--- matlab_validation/get_vco.m
+++
@@ -1,8 +0,0 @@
-function [ vco ] = get_vco( frac_num )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-vco = (25*(96 + frac_num/(2^24)) - 2400)*1e6;
-
-end
-
